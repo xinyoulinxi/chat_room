2faba2ae7d81e0a0cb8d48ca780ccd1b3a87285c
ylvoid <1135479396@qq.com>
2024-04-17 15:44:22 +0800

    add response with json data

--- utils/net_utils.go
+++ utils/net_utils.go
@@ -12,6 +12,19 @@ func GetReturnMessageJson(errorCode int, message string) []byte {
 	return jsonMsg
 }
 
+func GetReturnMessageJsonWithData(errorCode int, message string, data []byte) []byte {
+	jsonMsg, _ := json.Marshal(chat_type.ReturnMessage{ErrorCode: errorCode, Message: message, Data: data})
+	return jsonMsg
+}
+
+func WriteResponseWithData(w http.ResponseWriter, code int, message string, data []byte) {
+	slog.Info("WriteResponse", "code", code, "message", message)
+	_, err := w.Write(GetReturnMessageJsonWithData(code, message, data))
+	if err != nil {
+		return
+	}
+}
+
 func WriteResponse(w http.ResponseWriter, code int, message string) {
 	slog.Info("WriteResponse", "code", code, "message", message)
 	_, err := w.Write(GetReturnMessageJson(code, message))
