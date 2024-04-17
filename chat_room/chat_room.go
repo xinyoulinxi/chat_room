24b4845479a74b3ce32be18676195161d45e40f7
ylvoid <1135479396@qq.com>
2024-04-17 17:27:01 +0800

    Update chat_room.go

--- chat_room/chat_room.go
+++ chat_room/chat_room.go
@@ -48,7 +48,7 @@ func HistoryMessagesHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	// 检查请求方法是否为POST
 	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		utils.WriteResponse(w, chat_type.ErrorInvalidInput, "Invalid request method")
 		return
 	}
 
@@ -96,6 +96,7 @@ func HistoryMessagesHandler(w http.ResponseWriter, r *http.Request) {
 	// 将chatRoom.Messages转换成json字符串
 	jsonMsg, err := json.Marshal(messages)
 	if err != nil {
+		utils.WriteResponse(w, chat_type.ErrorCodeFail, "fail to fetch messages")
 		slog.Error("Failed to convert message to JSON", "error", err)
 		return
 	}
@@ -103,13 +104,59 @@ func HistoryMessagesHandler(w http.ResponseWriter, r *http.Request) {
 	utils.WriteResponseWithData(w, chat_type.ErrorCodeSuccess, "Success", jsonMsg)
 }
 
-func ChatRoomListHandler(w http.ResponseWriter, _ *http.Request) {
-	// userid := r.URL.Query().Get("userid")
-	// if(userid == "") {
-	//	w.Write(chat_type.GetReturnMessageJson(1, "Invalid user id"))
-	//	return
-	// }
+func UploadFilehandler(w http.ResponseWriter, r *http.Request) {
+	// 确保关闭请求体
+	defer r.Body.Close()
+	// 检查请求方法是否为POST
+	if r.Method != http.MethodPost {
+		utils.WriteResponse(w, chat_type.ErrorInvalidInput, "Invalid request method")
+		return
+	}
 
+	// 解析请求体
+	var loginData struct {
+		UserId   string `json:"userId"`
+		FileName string `json:"filename"`
+		Data     string `json:"data,omitempty"` // Base64-encoded data
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&loginData)
+	if err != nil {
+		utils.WriteResponse(w, chat_type.ErrorCodeFail, "Failed to parse request body")
+		return
+	}
+	userId := loginData.UserId
+	data := loginData.Data
+	fileName := loginData.FileName
+
+	if data != "" {
+		if len(data) > 1024*1024*20 {
+			utils.WriteResponse(w, chat_type.ErrorInvalidInput, "File too large")
+			return
+		}
+	}
+	filePath, fileType, err := utils.SaveFile(fileName, &data)
+	if err != nil {
+		utils.WriteResponse(w, chat_type.ErrorInvalidInput, "Failed to save file")
+		return
+	}
+	var fileStruct struct {
+		FilaPath string `json:"filePath"`
+		FileType string `json:"fileType"`
+	}
+	fileStruct.FilaPath = filePath
+	fileStruct.FileType = fileType
+	// file path 转换为json
+	filePathJson, err := json.Marshal(fileStruct)
+	if err != nil {
+		utils.WriteResponse(w, chat_type.ErrorInvalidInput, "Failed to marshal file path")
+		return
+	}
+	utils.WriteResponseWithData(w, 0, "Success", filePathJson)
+	slog.Info("uploadFilehandler", "userId", userId)
+}
+
+func ChatRoomListHandler(w http.ResponseWriter, _ *http.Request) {
 	roomList := ListChatRoom()
 	slog.Info("ChatRoomListHandler", "chatRoomList", roomList)
 	// Convert chat room list to JSON
