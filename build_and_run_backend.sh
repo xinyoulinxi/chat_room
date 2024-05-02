a6dd826861d3c619d133238decb742069f6f8e1e
ylvoid <ylvoid@qq.com>
2024-05-02 22:13:42 +0800

    more logs

--- build_and_run_backend.sh
+++ build_and_run_backend.sh
@@ -1,4 +1,8 @@
 go build -o out/
-# run web server in background
-nohup ./out/web_server > ./out/out.text  2>&1  & 
+# run web server in backgroud
+# Get the current date and time in the format you specified
+current_date=$(date +"%Y_%m_%d_%H_%M_%S")
+mkdir -p ./out/logs
+# Run your web server in the background and redirect output to a file named with the current date and time
+nohup ./out/web_server > ./out/logs/${current_date}_out.txt 2>&1 &
 
